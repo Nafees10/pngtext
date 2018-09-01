b88b1dd74cf76af92f3424762660a8b094c350d6
Nafees10 <nafeeshassan16@gmail.com>
2018-09-01 17:55:35 +0500

    Data types to store size is now uint, because it can't store more than 16 megs
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

--- source/lib.d
+++ source/lib.d
@@ -17,19 +17,9 @@ version(lib){
 			data);
 	}
 
-	/// returns the number of bytes that can be stored when the image is "saturated", i.e, capacity at zero quality
-	extern (C) uinteger pngCapacityMax(char* filename){
-		return calculatePngCapacity (cast(string)fromStringz(filename), 8);
-	}
-
-	/// returns the number of bytes that can be stored when at low quality
-	extern (C) uinteger pngCapacityLowQ(char* filename){
-		return calculatePngCapacity (cast(string)fromStringz(filename), 4);
-	}
-
-	/// returns the number of bytes that can be stored at high quality
-	extern (C) uinteger pngCapacityHighQ(char* filename){
-		return calculatePngCapacity (cast(string)fromStringz(filename), 2);
+	/// returns the number of bytes that can be stored
+	extern (C) uint pngCapacity(char* filename, ubyte density){
+		return calculatePngCapacity (cast(string)fromStringz(filename), desnity);
 	}
 
 	/// returns a float number, which tells the quality of the output image
@@ -37,7 +27,7 @@ version(lib){
 	/// the number is the average number of bits used per one byte (each pixel = 4 bytes). Only bytes from pixels which are used to hold
 	/// data are considered in the calcualtion, untouched pixels are not counted as they remain as-they-were;
 	/// the number can be at max 8 (saturated image), lowest 0 (no data & highest quality). Lower means higher quality.
-	extern (C) float getQuality(char* filename, uinteger dataLength){
+	extern (C) float getQuality(char* filename, uint dataLength){
 		// get the optimum quality assoc_array
 		uinteger[ubyte] optimumQ = calculateOptimumDensity(cast(string)fromStringz(filename) , dataLength);
 		// The formula is (index * value at index, for each index) / (total used bytes, = sum of values at all indexes)
