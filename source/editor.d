3a75c957e512feba58e831eb6697e74ed2a3b92c
Nafees10 <nafeeshassan16@gmail.com>
2020-11-29 00:51:55 +0500

    Redesdinged  the text editor to have more features, using the newer qui version

--- source/editor.d
+++ source/editor.d
@@ -9,102 +9,336 @@ import qui.widgets;
 
 import std.stdio;
 import std.conv : to;
+import std.path;
 
-/// The in-terminal text editor using qui.widgets.MemoWidget
-class Editor{
+/// shortcuts text
+private enum STATUSBAR_SHORTCUTS = "^C - Exit  ^O - Save;";
+/// prefix text for file saved label
+private enum STATUSBAR_FILESAVED_PREFIX = "File saved to ";
+/// prefix text for quality label
+private enum STATUSBAR_QUALITY_PREFIX = "Quality: ";
+/// prefix text for number of bytes label
+private enum STATUSBAR_BYTECOUNT_PREFIX = "Characters/Max/Limit: ";
+/// text for when file failed to save
+private enum STATUSBAR_FILESAVE_ERROR = "Error saving file";
+/// time (msecs) until "File saved to: ..." & errors disappears
+private enum ERROR_DISAPPEAR_TIME = 500;
+
+/// The top title display
+private class TitleWidget : QLayout{
 private:
-	/// the terminal
-	QTerminal _terminal;
-	/// the text editor
-	MemoWidget _editor;
-	/// to occupy space on left of 
-	SplitterWidget _statusBarLeft;
-	/// shows the shortcut keys, sits next to _statusLabel
-	TextLabelWidget _shortcutLabel;
-	/// Contains the and _shortcutLabel
-	QLayout _statusBar;
-	/// filename of original png
-	string _inputPng;
-	/// filename of output png
-	string _outputPng;
+	/// empty space on left
+	SplitterWidget _leftSplitter;
+	/// empty space on right
+	SplitterWidget _rightSplitter;
+	/// text
+	TextLabelWidget _titleLabel;
+	/// background color
+	Color _bgColor;
+	/// text color
+	Color _textColor;
 public:
 	/// constructor
-	/// 
-	/// if text only has to be displayed, use readOnly=true, and no need to specify saveAs, just leave it blank
-	this(string image, string saveAs, bool readOnly=false){
-		_inputPng = image;
-		_outputPng = saveAs;
-		// setup the terminal
-		_terminal = new QTerminal(QLayout.Type.Vertical);
-		_statusBar = new QLayout(QLayout.Type.Horizontal);
-		_editor = new MemoWidget(!readOnly);
-		_statusBarLeft = new SplitterWidget();
-		_shortcutLabel = new TextLabelWidget();
-		// set up each widget
-		// first comes the editor:
-		_editor.wantsTab = false;
-		//_editor.lines.loadArray(separateLines((cast(string)cast(char[])readDataFromPng(_inputPng)).to!dstring));//load the lines TODO
-		// now comes the _statusLabel
-		// now the _shortcutLabel
-		_shortcutLabel.textColor = DEFAULT_BG;
-		_shortcutLabel.backgroundColor = DEFAULT_FG;
-		_shortcutLabel.caption = "Ctrl+C - Save & Exit";
-		_shortcutLabel.size.maxWidth = _shortcutLabel.caption.length;
-		// put both of these in _statusBar, and set it up too
-		_statusBar.addWidget([_statusBarLeft, _shortcutLabel]);
-		_statusBar.size.maxHeight = 1;
-		_statusBar.size.minHeight = 1;
-		// put all those in QTerminal
-		_terminal.addWidget([_editor, _statusBar]);
-		// register all widgets
-		_terminal.registerWidget([_editor, _statusBar, _statusBarLeft, _shortcutLabel]);
-		// and its done, ready to start*/
+	this(dstring text, Color fg = Color.black, Color bg = Color.white){
+		super(QLayout.Type.Horizontal);
+		_leftSplitter = new SplitterWidget();
+		_rightSplitter = new SplitterWidget();
+		_titleLabel = new TextLabelWidget();
+		this.text = text;
+		_leftSplitter.color = bg;
+		_rightSplitter.color = bg;
+		_titleLabel.backgroundColor = bg;
+		_titleLabel.textColor = fg;
+		this.size.maxHeight = 1;
+		this.size.minHeight = 1;
+		// exaggerate the sizeRatio of _titleLabel, so splitters can become as small as needed
+		_titleLabel.sizeRatio = 1000;
+		this.addWidget([_leftSplitter, _titleLabel, _rightSplitter]);
 	}
-	/// destructor
 	~this(){
-		.destroy(_terminal);
-		.destroy(_editor);
-		.destroy(_statusBar);
-		.destroy(_statusBarLeft);
-		.destroy(_shortcutLabel);
+		.destroy(_leftSplitter);
+		.destroy(_rightSplitter);
+		.destroy(_titleLabel);
 	}
-	/// runs the editor
-	/// 
-	/// Returns: false if there were errors(s)
-	bool run(){
-		_terminal.run;
-		// save
-		const dstring[] lines = _editor.lines.toArray;
-		string[] utf8Lines;
-		utf8Lines.length = lines.length;
-		foreach (i, line; lines){
-			utf8Lines[i] = line.to!string;
-		}
-		ubyte[] data;
-		uinteger writeTo = 0;
-		foreach (line; utf8Lines){
-			data.length += line.length+1;
-			foreach (ch; line){
-				data[writeTo] = cast(ubyte)ch;
-				writeTo++;
-			}
-			data[writeTo] = '\n';
-			writeTo++;
-		}
-		const string[] errors;// = writeDataToPng(_inputPng, _outputPng, data); TODO
-		if (errors.length){
-			stderr.writeln("Errors while writing to image:");
-			foreach (err; errors){
-				stderr.writeln(err);
-			}
-			return false;
-		}
-		return true;
+	/// text displayed
+	@property dstring text(){
+		return _titleLabel.caption;
 	}
-
+	/// ditto
+	@property dstring text(dstring newVal){
+		_titleLabel.caption = newVal;
+		_titleLabel.size.maxWidth = newVal.length;
+		this.resizeEvent();
+		return newVal;
+	}
 }
 
-/// reads a single string into string[], separating the lines
+/// To display a log with a title
+private class LogPlusPlusWidget : QLayout{
+private:
+	/// to display a title at top of errors
+	TitleWidget _title;
+	/// log to show the errors
+	LogWidget _log;
+public:
+	/// constructor
+	this(dstring title, uint lines, Color fg, Color bg){
+		super(QLayout.Type.Vertical);
+		_title = new TitleWidget(title, bg, fg);
+		_log = new LogWidget(lines);
+		_log.textColor = fg;
+		_log.backgroundColor = bg;
+		_log.size.minHeight = lines;
+		_log.size.maxHeight = lines;
+		this.size.minHeight = lines+1;
+		this.size.maxHeight = lines+1;
+		this.addWidget([_title, _log]);
+	}
+	~this(){
+		.destroy(_title);
+		.destroy(_log);
+	}
+	/// clears error messages
+	void clear(){
+		_log.clear;
+	}
+	/// adds an error message
+	void add(dstring message){
+		_log.add(message);
+	}
+	/// whether this widget is shown or not
+	override @property bool show(bool newVal){
+		return super.show = newVal;
+	}
+	/// ditto
+	override @property bool show(){
+		return super.show;
+	}
+}
+
+/// A status bar. Displays TextLabelWidgets with spaces in between.
+private class StatusBarWidget : QLayout{
+private:
+	TextLabelWidget[] _label;
+	SplitterWidget[] _splitter;
+public:
+	this(ubyte count, Color fg, Color bg){
+		assert(count, "count cannot be 0");
+		super(QLayout.Type.Horizontal);
+		_label.length = count;
+		_splitter.length = count -1;
+		_label[0] = new TextLabelWidget();
+		_label[0].sizeRatio = 1000;
+		_label[0].textColor = fg;
+		_label[0].backgroundColor = bg;
+		this.addWidget(_label[0]);
+		foreach (i; 1 .. count){
+			_splitter[i-1] = new SplitterWidget();
+			_splitter[i-1].color = bg;
+			this.addWidget(_splitter[i-1]);
+			_label[i] = new TextLabelWidget();
+			_label[i].sizeRatio = 1000;
+			_label[i].textColor = fg;
+			_label[i].backgroundColor = bg;
+			this.addWidget(_label[i]);
+		}
+		this.size.maxHeight = 1;
+	}
+	~this(){
+		foreach (widget; _label)
+			.destroy(widget);
+		foreach(widget; _splitter)
+			.destroy(widget);
+	}
+	/// Sets text of a label
+	void setText(uint label, dstring newText){
+		_label[label].caption = newText;
+		_label[label].size.maxWidth = newText.length;
+		requestResize();
+	}
+}
+
+/// Memo with extra functions
+private class MemoPlusPlusWidget : MemoWidget{
+public:
+	this(){
+		super(true);
+	}
+	/// Calculates number of bytes required to store the characters typed
+	uint bytesCount(){
+		uint r;
+		dstring[] linesArray = this.lines.toArray;
+		foreach (line; linesArray)
+			r += to!string(line).length;
+		r += lines.length; // for the \n at end of each line
+		return r;
+	}
+	/// Returns: the contents of memo as a single string
+	string getString(){
+		dstring[] linesArray = this.lines.toArray;
+		uint len;
+		// first calculate total length needed
+		foreach (line; linesArray)
+			len += to!string(line).length;
+		len += linesArray.length; // for the \n at end of each line
+		char[] r;
+		r.length = len;
+		for (uint lineno, writeIndex; lineno < linesArray.length; lineno ++){
+			string line = linesArray[lineno].to!string;
+			r[writeIndex .. writeIndex + line.length+1] = line~'\n';
+			writeIndex += line.length+1;
+		}
+		return cast(string)r;
+	}
+}
+
+/// The text editor app
+public class App : QTerminal{
+private:
+	/// path of output image
+	string _outputPath;
+	/// the PNGText class instance
+	PNGText _imageMan;
+	/// Title at top
+	TitleWidget _title;
+	/// memo for editing
+	MemoPlusPlusWidget _memo;
+	/// error messages
+	LogPlusPlusWidget _log;
+	/// status bar
+	StatusBarWidget _statusBar;
+
+	/// stores how many bytes can be stored at what densities
+	int[ubyte] _capacity;
+	/// how many msecs until the shortcut keys are shown again in status bar instead of "File saved..."
+	int _msecsUntilReset;
+	
+	/// updates status bar
+	void updateStatusBar(){
+		immutable int bytesCount = _memo.bytesCount;
+		ubyte density = 0;
+		int max = 0;
+		foreach (d; DENSITIES){
+			if (_capacity[d] >= bytesCount){
+				density = d;
+				max = _capacity[d];
+				break;
+			}
+		}
+		_statusBar.setText(0, 
+			STATUSBAR_QUALITY_PREFIX~(density == 0 ? "OVER CAPACITY" : _imageMan.qualityName(density).to!dstring));
+		// now for bytes count
+		_statusBar.setText(1, STATUSBAR_BYTECOUNT_PREFIX~bytesCount.to!dstring~"/"~max.to!dstring~"/"~
+			_capacity[DENSITIES[$-1]].to!dstring);
+		// now shortcuts or leave it at that "file saved"
+		if (_msecsUntilReset <= 0)
+			_statusBar.setText(2, STATUSBAR_SHORTCUTS);
+	}
+
+	/// initializes _capacity
+	void initCapacity(){
+		foreach (i; DENSITIES){
+			_capacity[i] = _imageMan.capacity(i);
+		}
+	}
+	/// loads image, initializes _imageMan.
+	/// 
+	/// Displays error in _log if any
+	void initImageMan(){
+		try{
+			_imageMan.load();
+			_imageMan.decode();
+			_imageMan.filename = _outputPath;
+		}catch (Exception e){
+			_log.clear;
+			_log.add("Error loading image:");
+			_log.add(e.msg.to!dstring);
+			_log.show = true;
+			_msecsUntilReset = int.max;
+		}
+	}
+	/// Reads data from _imageMan to memo
+	void initMemo(){
+		_memo.lines.loadArray((cast(char[])_imageMan.data).to!dstring.separateLines);
+	}
+	/// Writes data from memo to _imageMan and then saves
+	void save(){
+		string data = _memo.getString();
+		_imageMan.data = cast(ubyte[])cast(char[])data;
+		_msecsUntilReset = int.max;
+		try{
+			_imageMan.encode();
+			_imageMan.save();
+			_statusBar.setText(2, STATUSBAR_FILESAVED_PREFIX~_outputPath.to!dstring);
+		}catch (Exception e){
+			_log.clear;
+			_log.add("Error saving image:");
+			_statusBar.setText(2, STATUSBAR_FILESAVE_ERROR);
+			_log.add(e.msg.to!dstring);
+			_log.show = true;
+			_msecsUntilReset = int.max;
+		}
+	}
+protected:
+	override void timerEvent(uinteger msecs){
+		//super.timerEvent(msecs); // HACK: THIS IS COMMENTED OUT AS A HACK
+		if (_msecsUntilReset > 0)
+			_msecsUntilReset -= msecs;
+		if (_log.show && _msecsUntilReset <= 0)
+			_log.show = false;
+		updateStatusBar();
+		requestUpdate();
+	}
+	override void keyboardEvent(KeyboardEvent key){
+		super.keyboardEvent(key);
+		// nothing happens if there was an error loading
+		if (!_imageMan.imageLoaded)
+			return;
+		if (_msecsUntilReset > ERROR_DISAPPEAR_TIME)
+			_msecsUntilReset = ERROR_DISAPPEAR_TIME;
+		if (key.isCtrlKey && key.key == KeyboardEvent.CtrlKeys.CtrlO){
+			save();
+		}
+	}
+public:
+	/// Constructor
+	this(string imagePath, string outputPath){
+		// construct the interface
+		_title = new TitleWidget(baseName(imagePath).to!dstring);
+		_memo = new MemoPlusPlusWidget();
+		_log = new LogPlusPlusWidget("Error Messages", 3, Color.red, Color.black);
+		_statusBar = new StatusBarWidget(3, Color.white, Color.black);
+		// arrange em
+		this.addWidget([_title, _memo, _log, _statusBar]);
+		// make _imageMan
+		_imageMan = new PNGText();
+		_imageMan.filename = imagePath;
+		_outputPath = outputPath;
+		/// UGLY HACK BELOW.
+		/// HACK: QTerminal does not call it's own timerEvent, as a workaround, get some widget to call it
+		_memo.onTimerEvent = delegate(QWidget, uinteger msecs){
+			this.timerEvent(msecs);
+			return false;
+		};
+	}
+	~this(){
+		.destroy(_imageMan);
+		.destroy(_title);
+		.destroy(_memo);
+		.destroy(_log);
+		.destroy(_statusBar);
+	}
+	/// starts the app
+	override void run(){
+		initImageMan();
+		initMemo();
+		if (_imageMan.imageLoaded)
+			initCapacity;
+		super.run();
+	}
+}
+
+/// reads a single dstring into dstring[], separating the lines
 private dstring[] separateLines(dstring s){
 	dstring[] r;
 	for(uinteger i = 0, readFrom = 0; i < s.length; i ++){
